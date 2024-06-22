ad9e81d287bb8914e2410d41991ff0404cfeec8e
Gerardo Rivera <61441440+gerardoriveradoesstuff@users.noreply.github.com>
2024-06-22 03:58:31 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+<script
+  async
+  crossorigin="anonymous"
+  data-clerk-publishable-key="pk_test_d2l0dHktZ2lyYWZmZS01MC5jbGVyay5hY2NvdW50cy5kZXYk"
+  src="https://witty-giraffe-50.clerk.accounts.dev/npm/@clerk/clerk-js@latest/dist/clerk.browser.js"
+  type="text/javascript"
+></script>
